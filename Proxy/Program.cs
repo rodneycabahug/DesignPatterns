6726ae1b09f60350e6eafb5cb331e8a9436d9c00
Rodney Cabahug <rodney.cabahug@gmail.com>
2022-05-04 22:57:17 +1200

    feat: Add Proxy

---
+++ Proxy/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://www.dofactory.com/net/proxy-design-pattern
+
+class Program
+{
+    public static void Main()
+    {
+        var subject = new Proxy();
+        subject.Operation();
+    }
+}
+
+internal abstract class Subject
+{
+    internal abstract void Operation();
+}
+
+internal class RealSubject : Subject
+{
+    internal override void Operation() =>
+        Console.WriteLine("{0} {1}", nameof(RealSubject), nameof(Operation));
+}
+
+internal class Proxy : Subject
+{
+    private Lazy<RealSubject> _realSubject = new(() => new RealSubject());
+
+    internal override void Operation() => _realSubject.Value.Operation();
+}
