489b670e562f475ed9a044c5c1eb471444c613e6
Rodney Cabahug <rodney.cabahug@gmail.com>
2022-05-04 20:39:14 +1200

    feat: Add Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿// https://www.dofactory.com/net/singleton-design-pattern
+
+class Program
+{
+    public static void Main()
+    {
+        Singleton instanceOne = Singleton.Instance;
+        Singleton instanceTwo = Singleton.Instance;
+
+        if (instanceOne == instanceTwo)
+            Console.WriteLine("instanceOne and instanceTwo are the same instances.");
+    }
+}
+
+internal class Singleton
+{
+    private static readonly Singleton _instance = new Singleton();
+
+    public static Singleton Instance
+    { 
+        get => _instance;
+    }
+
+    private Singleton()
+    {
+    }
+}
