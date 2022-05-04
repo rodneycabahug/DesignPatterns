d0f0a9e518546109354ce15409596831ac88e488
Rodney Cabahug <rodney.cabahug@gmail.com>
2022-05-04 23:34:00 +1200

    feat: Add Command

---
+++ Command/Program.cs
@@ -0,0 +1,53 @@
+﻿// https://www.dofactory.com/net/command-design-pattern
+
+class Program
+{
+    public static void Main()
+    {
+        var invoker = new Invoker(new ConcreteCommand(new Receiver()));
+        invoker.ExecuteCommand();
+    }
+}
+
+internal class Invoker
+{
+    private readonly Command _command;
+
+    internal Invoker(Command command)
+    {
+        _command = command;
+    }
+
+    internal void ExecuteCommand() => _command.Execute();
+}
+
+internal class ConcreteCommand : Command
+{
+    internal ConcreteCommand(Receiver reciever) : base(reciever)
+    {
+    }
+
+    internal override void Execute()
+    {
+        Console.WriteLine("{0} {1}", nameof(ConcreteCommand), nameof(Execute));
+        base.Execute();
+        Console.WriteLine("{0} {1}", nameof(ConcreteCommand), nameof(Execute));
+    }
+}
+
+internal abstract class Command
+{
+    private readonly Receiver _reciever;
+
+    protected Command(Receiver reciever)
+    {
+        _reciever = reciever;
+    }
+
+    internal virtual void Execute() => _reciever.Operation();
+}
+
+internal class Receiver
+{
+    internal void Operation() => Console.WriteLine("{0} {1}", nameof(Receiver), nameof(Operation));
+}
