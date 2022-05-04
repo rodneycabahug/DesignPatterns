39232164bc634986ac7f12323c7891d626aa97d7
Rodney Cabahug <rodney.cabahug@gmail.com>
2022-05-04 21:13:42 +1200

    feat: Add Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://www.dofactory.com/net/adapter-design-pattern
+
+class Program
+{
+    public static void Main()
+    {
+        Target target = new Adapter();
+        target.Request();
+    }
+}
+
+internal class Adaptee
+{
+    public void TargetIncompatibleRequest() =>
+        Console.WriteLine("Called TargetIncompatibleRequest");
+}
+
+internal class Adapter : Target
+{
+    private readonly Adaptee _adaptee = new Adaptee();
+
+    public void Request() => _adaptee.TargetIncompatibleRequest();
+}
+
+internal interface Target
+{
+    void Request();
+}
